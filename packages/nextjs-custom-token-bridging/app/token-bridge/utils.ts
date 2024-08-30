6d5e7c0ee130e9ba5cfaa9050c56c4f19f219f57
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-08-30 16:18:28 +0700

    add: getDecimals function

--- packages/nextjs-custom-token-bridging/app/token-bridge/utils.ts
+++ packages/nextjs-custom-token-bridging/app/token-bridge/utils.ts
@@ -27,18 +27,36 @@ export async function balanceOfERC20(
   return balanceERC20;
 }
 
-export async function checkBalanceERC20(contractAddress: string, account: string, provider: Provider, amount: BigNumber) {
-    const balanceERC20: BigNumber = await balanceOfERC20(contractAddress, account, provider);
+export async function checkBalanceERC20(
+  contractAddress: string,
+  account: string,
+  provider: Provider,
+  amount: BigNumber
+) {
+  const balanceERC20: BigNumber = await balanceOfERC20(
+    contractAddress,
+    account,
+    provider
+  );
 
-    if (amount.gt(balanceERC20)){
-        throw new Error(
-            `Insufficient Balance ${ethers.utils.formatUnits(
-              amount
-            )} > ${ethers.utils.formatUnits(
-              balanceERC20
-            )}`
-        );
-    }
+  if (amount.gt(balanceERC20)) {
+    throw new Error(
+      `Insufficient Balance ${ethers.utils.formatUnits(
+        amount
+      )} > ${ethers.utils.formatUnits(balanceERC20)}`
+    );
+  }
+}
+
+export async function getDecimals(
+  contractAddress: string,
+  provider: Provider,
+  amount: BigNumber
+): Promise<BigNumber> {
+  const abi = ["function decimals() public view virtual returns (uint8)"];
+  const contract = new Contract(contractAddress, abi, provider);
+  const decimals = await contract.decimals();
+  return amount.mul(BigNumber.from(10).pow(decimals));
 }
 
 export function percentIncrease(
@@ -49,29 +67,29 @@ export function percentIncrease(
 }
 
 export async function transferFrom(
-    contractAddress: string,
-    signer: Signer,
-    from: string,
-    to: string,
-    value: BigNumber
-  ) {
-    const iface = new ethers.utils.Interface([
-      "function transferFrom(address from, address to, uint256 value) external returns (bool)",
-      "event Transfer(address indexed from, address indexed to, uint256 value)",
-    ]);
-    const contract = new ethers.Contract(contractAddress, iface, signer);
-    const tx = await contract.transferFrom(from, to, value);
-    const receipt = await tx.wait();
-    for (const log of receipt.logs) {
-      const mLog = iface.parseLog(log);
-      if (mLog && mLog.name === "Transfer") {
-        console.log(`TransferFrom L3 successfully! Tx hash: ${tx.hash}`);
-      }
+  contractAddress: string,
+  signer: Signer,
+  from: string,
+  to: string,
+  value: BigNumber
+) {
+  const iface = new ethers.utils.Interface([
+    "function transferFrom(address from, address to, uint256 value) external returns (bool)",
+    "event Transfer(address indexed from, address indexed to, uint256 value)",
+  ]);
+  const contract = new ethers.Contract(contractAddress, iface, signer);
+  const tx = await contract.transferFrom(from, to, value);
+  const receipt = await tx.wait();
+  for (const log of receipt.logs) {
+    const mLog = iface.parseLog(log);
+    if (mLog && mLog.name === "Transfer") {
+      console.log(`TransferFrom L3 successfully! Tx hash: ${tx.hash}`);
     }
-    return JSON.stringify({
-        txHash: tx.hash,
-        from: from,
-        to: to,
-        value: value
-    });
   }
+  return JSON.stringify({
+    txHash: tx.hash,
+    from: from,
+    to: to,
+    value: value,
+  });
+}
