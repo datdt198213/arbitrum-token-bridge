d4545591cc42cfe5a952a891d141bc0e31941625
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-08-30 16:54:31 +0700

    add: balance of component

--- packages/nextjs-custom-token-bridging/app/layout.tsx
+++ packages/nextjs-custom-token-bridging/app/layout.tsx
@@ -4,6 +4,7 @@ import { Deposit } from "./components/deposit";
 import { Withdraw } from "./components/withdraw";
 import { Claim } from "./components/claim";
 import { Transfer } from "./components/transfer";
+import { BalanceOf } from "./components/balance";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,6 +16,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
           <Withdraw />
           <Claim />
           <Transfer />
+          <BalanceOf />
         </Providers>
       </body>
     </html>
